7947195e7af390979f00deff93af64b65d25b412
Eric Martinez <emartinez@usgs.gov>
2014-02-20 15:02:27 -0700

    Reverted to actual data set rather than using legacy proxy.

--- src/htdocs/dataminer.php
+++ src/htdocs/dataminer.php
@@ -1,14 +1,13 @@
 <?php
 	include_once 'functions.inc.php';
-	// include_once '../conf/config.inc.php';
-	// include_once '../lib/classes/GriddedDataFactory.class.php';
+	include_once '../conf/config.inc.php';
+	include_once '../lib/classes/GriddedDataFactory.class.php';
 
 	$latitude  = param('latitude', '10');
 	$longitude = param('longitude', '10');
 
 	header("Content-Type: application/json");
 
-	// $factory = new GriddedDataFactory($DB);
-	// print json_encode($factory->compute($latitude, $longitude));
-	print file_get_contents('http://geohazards.usgs.gov/designmaps/ww/dataminer.php?latitude='.$latitude.'&longitude='.$longitude);
+	$factory = new GriddedDataFactory($DB);
+	print json_encode($factory->compute($latitude, $longitude));
 ?>
